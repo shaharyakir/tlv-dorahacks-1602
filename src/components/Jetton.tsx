bccf3a51585932fef808c22f8291950730011e4f
Shahar Yakir <shahar.yakir@gmail.com>
2023-02-16 12:59:06 +0200

    burned

--- src/components/Jetton.tsx
+++ src/components/Jetton.tsx
@@ -1,17 +1,21 @@
 import { beginCell, toNano, Address, Cell, fromNano } from "ton";
 import { useTonConnect } from "../hooks/useTonConnect";
 import { useFaucetJettonContract } from "../hooks/useFaucetJettonContract";
+import { useState } from "react";
 import {
   Card,
   FlexBoxCol,
   FlexBoxRow,
   Button,
   Ellipsis,
+  Input,
 } from "./styled/styled";
 
 export function Jetton() {
   const { connected } = useTonConnect();
-  const { mint, jettonWalletAddress, balance } = useFaucetJettonContract();
+  const { burn, mint, jettonWalletAddress, balance } =
+    useFaucetJettonContract();
+  const [amount, setAmount] = useState('0');
 
   return (
     <Card title="Jetton">
@@ -32,6 +36,23 @@ export function Jetton() {
           }}
         >
           Get jettons from faucet
+        </Button>
+        <FlexBoxRow>
+          <label>Burn amount </label>
+          <Input
+            style={{ marginRight: 8 }}
+            value={amount}
+            type="number"
+            onChange={(e) => setAmount(e.target.value)}
+          ></Input>
+        </FlexBoxRow>
+        <Button
+          disabled={!connected}
+          onClick={async () => {
+            burn(toNano(amount));
+          }}
+        >
+          🔥
         </Button>
       </FlexBoxCol>
     </Card>
--- src/contracts/faucetJettonWallet.ts
+++ src/contracts/faucetJettonWallet.ts
@@ -20,4 +20,16 @@ export default class FaucetJettonWallet implements Contract {
     readonly address: Address,
     readonly init?: { code: Cell; data: Cell }
   ) {}
+
+  async sendBurn(provider: ContractProvider, via: Sender, amount: bigint) {
+    provider.internal(via, {
+      value: toNano("0.05"),
+      body: beginCell()
+        .storeUint(0x595f07bc, 32)
+        .storeUint(0, 64)
+        .storeCoins(amount)
+        .storeAddress(via.address!)
+        .endCell(),
+    });
+  }
 }
